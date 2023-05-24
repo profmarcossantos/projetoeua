47d51bce4e2b238ecc14b9b647ba159e75291957
Marcos Santos <marcos.santos@atitus.edu.br>
2023-05-24 20:17:02 -0300

    Primeira Versão

---
+++ funcoes.py
@@ -0,0 +1,7 @@
+import os
+import time
+def limparTela():
+    os.system("cls")
+
+def aguardar(segundos):
+    time.sleep(segundos)
---
+++ main.py
@@ -0,0 +1,34 @@
+from funcoes import limparTela, aguardar
+while True:
+    limparTela()
+    print("(0) Sair")
+    print("(1) Incluir Aluno")
+    print("(2) Mostrar Lista")
+    opcao = input()
+    if opcao == "0":
+        break
+    elif opcao == "1":
+        nome = input("Nome: ")
+        email = input("E-mail:")
+        arquivo = open("bd.atitus","a") #w  write # r read # a append
+        arquivo.write(nome+" "+email+"\n")
+        arquivo.close()
+        print("Dados Salvos")
+        aguardar(1)
+    elif opcao == "2":
+        try:
+            arquivo = open("bd.atitus","r")
+            dados = arquivo.read()
+            print(dados)
+            arquivo.close()
+            aguardar(5)
+        except:
+            print("Banco de Dados não localizado!")
+            print("Estamos criando um para vc!")
+            arquivo = open("bd.atitus","w")
+            arquivo.close()
+            aguardar(1)
+    else:
+        print("Opção Inválida!")
+        aguardar(2)
+
